75935fc524eab5efc05f9dba8f765e6ad729287c
raghavarahul <raghavak235@gmail.com>
2024-05-26 10:28:42 -0400

    Adding Even or Odd Number

---
+++ odd_or_even.py
@@ -0,0 +1,12 @@
+
+
+def odd_or_even(n):
+    if n > 0:
+        if n%2 == 0:
+            print("Even Number")
+        else:
+            print("Odd Number")
+    else:
+        print("Invalid Number")
+
+odd_or_even(n=3)
