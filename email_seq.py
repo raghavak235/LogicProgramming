98218d6025efa70792230f13a2884362260a7f2e
raghavarahul <raghavak235@gmail.com>
2024-05-28 20:40:24 -0400

    Adding the Email Matching

---
+++ email_seq.py
@@ -0,0 +1,15 @@
+# Email name should be starts with alphabet and should follw by number or
+# underscore.
+# It should contains either numbers or underscore finally ends with @gmail.com
+# only,
+# Then given email id is true otherwise false.
+# input ------> email id
+# constraint -> lowercase alphabet [a-z] followed by underscore or digit and
+# gmail.com
+# output -----> true or false
+
+import re
+
+def email_seq():
+    m = re.fullmatch('[a-z]+[0-9|_]@gmail[.]com',input())
+    print("true" if m != None else "false")
